0e794ba6b4d1708ca527611afbf29817e9050575
booskander <hannachiskander@live.ca>
2024-11-15 01:04:09 -0500

    changing playbook commands

--- terraform/setup_minikube.sh
+++ terraform/setup_minikube.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "Starting Minikube with two nodes..."
-# on macOS we would have to set the driver to docker 
-# therefore port forwarding is mandatory since docker has
-# limited network accessibility
 minikube start --driver=docker --nodes=2
 
 alias kubectl="minikube kubectl --"
 
 echo "Waiting for Minikube nodes to be ready..."
-sleep 30 
+sleep 30  # Adjust this wait time as needed to ensure nodes are up
 
+# Retrieve node names
 nodes=($(kubectl get nodes -o jsonpath='{.items[*].metadata.name}'))
 
 if [ ${#nodes[@]} -ge 2 ]; then
+  # Label the first node as development
   kubectl label node "${nodes[0]}" environment=development --overwrite
   echo "Labeled ${nodes[0]} as development"
 
+  # Label the second node as production
   kubectl label node "${nodes[1]}" environment=production --overwrite
   echo "Labeled ${nodes[1]} as production"
 else
